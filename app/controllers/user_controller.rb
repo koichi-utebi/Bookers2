314ac7677ecac2224dc4832adc1cc6882e7304f4
EC2 Default User <ec2-user@ip-172-31-24-223.ec2.internal>
2022-04-12 04:42:50 +0000

    動的ヘッダーの作成

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,17 @@
 class UserController < ApplicationController
   def index
+    @user = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction)
   end
 end
